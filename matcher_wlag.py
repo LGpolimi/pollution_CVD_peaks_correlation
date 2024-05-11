5c30d16ba61a54dabae630f9193e8c726c60e43f
tommasomattarelli <166012617+tommasomattarelli@users.noreply.github.com>
2024-05-11 18:25:05 +0200

    Update matcher_wlag.py

--- matcher_wlag.py
+++ matcher_wlag.py
@@ -7,26 +7,29 @@ from datetime import datetime
 def comparatore(dsPOLL, dsEMS, colonnadataPOLL, colonnadataEMS, lag_giorni):
     matching = 0
 
+    # Creare un set per tenere traccia delle date in dsEMS che hanno già un match
+    matched_dates_in_dsEMS = set()
+
     for index1, row1 in dsPOLL.iterrows():
+        data1 = datetime.strptime(row1[colonnadataPOLL], '%Y-%m-%d')
+
         for index2, row2 in dsEMS.iterrows():
+            # Verificare se la data in dsEMS è già stata associata
+            if index2 not in matched_dates_in_dsEMS:
+                data2 = datetime.strptime(row2[colonnadataEMS], '%Y-%m-%d')
 
-            data1 = datetime.strptime(row1[colonnadataPOLL], '%Y-%m-%d')
-            data2 = datetime.strptime(row2[colonnadataEMS], '%Y-%m-%d')
-
-            # Calcola la differenza in giorni tra le due date  --->  data limite compresa
-            differenza_giorni = abs((data1 - data2).days)
-            if differenza_giorni == lag_giorni:
-                matching += 1
-                # Puoi registrare il match trovato, ad esempio, aggiungendolo a una lista
-                # matches.append((index1, index2))  # Dove index1 e index2 sono gli indici dei picchi di POLL e EMS
-                break
+                differenza_giorni = abs((data1 - data2).days)
+                if differenza_giorni == lag_giorni:
+                    matching += 1
+                    # Contrassegnare la data in dsEMS come già associata
+                    matched_dates_in_dsEMS.add(index2)
+                    # Esci dal loop per evitare confronti multipli per la stessa data in dsPOLL
+                    break  # Esci dal loop interno
 
     if len(dsPOLL) == 0:
         kpi = 0
     else:
-        kpi = (matching / (len(dsPOLL))) * 100
-
-    ###########################################################################à
+        kpi = (matching / len(dsPOLL)) * 100
 
     return kpi, matching, len(dsPOLL), len(dsEMS)
 
