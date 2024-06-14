35feb52771f3a01a3f5e8e1d8f30175045e0837c
LG_Polimi <lorenzo.gianquintieri@polimi.it>
2024-06-14 18:04:32 +0200

    Added counter on arpa processing

--- arpa.py
+++ arpa.py
@@ -3,9 +3,15 @@ import geopandas as gpd
 import numpy as np
 from shapely.geometry import Point
 from geopy.distance import geodesic
+import os
 
 # sostituire i percorsi dei file in ingresso e della posizione in cui si vuole scaricare il file in uscita
-
+input_path = 'D:\\Lorenzo Documents\\Lorenzo\\Research Documents\\2023 11 -- Pollution&Health\\2404_Tesi_bachelor_POLL_CVD\\Data\\datasources\\'
+output_path = 'D:\\Lorenzo Documents\\Lorenzo\\Research Documents\\2023 11 -- Pollution&Health\\2404_Tesi_bachelor_POLL_CVD\\Data\\processing\\'
+if not os.path.isdir(input_path):
+    os.makedirs(input_path)
+if not os.path.isdir(output_path):
+    os.makedirs(output_path)
 
 def weighted_average(distances, values, max_radius=50):
     # Converti le distanze e i valori in array numpy
@@ -31,7 +37,7 @@ def weighted_average(distances, values, max_radius=50):
     return weighted_sum / np.sum(weights)
 
 
-misure = pd.read_csv('/Users/sofiatorricella/Desktop/polimi/progetto/stazioni arpa/Dati_sensori_aria_2018_2023.csv')
+misure = pd.read_csv(input_path + 'stazioni arpa/ARPA_NO2_171819.csv')
 
 
 misure = misure.dropna(subset=['Valore'])
@@ -45,7 +51,7 @@ media_misure['idSensore'] = media_misure['idSensore'].astype('int64')
 stazioni = gpd.GeoDataFrame(media_misure, geometry='geometry')
 
 # Definizione dei poligoni delle aree
-griglia = gpd.read_file('/Users/sofiatorricella/Desktop/scuola/sosi/polimi/progetto/griglie/LMB3A.shp')
+griglia = gpd.read_file(input_path + 'griglie/LMB3A.shp')
 
 # Converti la griglia in EPSG:4326 per calcolare i centroidi correttamente
 griglia = griglia.to_crs(epsg=4326)
@@ -57,7 +63,12 @@ unique_dates = stazioni['Data'].unique()
 # Calcola la media pesata dell'inquinamento per ciascun giorno per ogni area della griglia
 results = []
 
+n_dates = len(unique_dates)
+iti = 0
+
 for date in unique_dates:
+    iti = iti + 1
+    print('Working on',date,'processing =',(iti/n_dates)*100,'%')
     stazioni_filtered = stazioni[stazioni['Data'] == date]
 
     for area in griglia.itertuples():  # Itera su ogni riga del GeoDataFrame delle griglie
@@ -101,4 +112,4 @@ result_gdf.rename(columns={'Weighted Average Pollution': 'Valore'}, inplace=True
 result_gdf.drop(columns=['Index', 'Area'], inplace=True)
 
 
-result_gdf.to_file('/Users/sofiatorricella/Desktop/scuola/sosi/polimi/progetto/stazioni arpa/ARPA_NO2_2023.shp')
+result_gdf.to_file(output_path+'ARPA_NO2_2023.shp')
